e0669ea558809dd22cc8fa9a879a9008dfa77b4c
Dang Hao <62649837+haotamht@users.noreply.github.com>
2023-11-13 22:10:15 +0700

    done app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,11 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        externalNativeBuild {
+            cmake {
+                cppFlags += ""
+            }
+        }
         configurations.all {
             resolutionStrategy {
                 force("androidx.emoji2:emoji2-views-helper:1.3.0")
@@ -53,6 +58,12 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
         }
     }
 }
